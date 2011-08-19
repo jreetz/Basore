3391111efbe10c432e6b84ecae76b9503bc3056d
Ryan Leckey <leckey.ryan@gmail.com>
2011-08-19 00:25:25 -0700

    Defined more test scripts for <stdlib.h>.

--- test/c/stdlib/_Exit.c
+++ test/c/stdlib/_Exit.c
@@ -1,6 +1,6 @@
-/// \file test/c/stdlib/abort.c
+/// \file test/c/stdlib/_Exit.c
 ///
-/// \brief Provides test procedure for abort().
+/// \brief Provides test procedure for _Exit().
 ///
 /// \author Ryan Leckey (mehcode) leckey.ryan@gmail.com
 ///
@@ -11,28 +11,12 @@
 /// You should have received a copy of the CC0 Public Domain Dedication along with this software.
 /// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 #include <stdlib.h>
-#include <signal.h>
-#include <platform.h>
 
-// Signal handler for SIGABRT
-void abort_handler()
-{
-    // Just return success
-    __platform_exit(EXIT_SUCCESS);
-}
-
-// Test procedure for abort()
+// Test procedure for _Exit()
 int main()
 {
-    // Attempt to register signal handler
-    if (signal(SIGABRT, &abort_handler) != &abort_handler)
-    {
-        // Failed to register handler, bail
-        return EXIT_FAILURE;
-    }
-
-    // Attempt to abort
-    abort();
+    // Attempt to exit using _Exit
+    _Exit(EXIT_SUCCESS);
 
     // Return failure
     return EXIT_FAILURE;
